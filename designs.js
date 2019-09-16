cd76b39b2aea115b1ad8f8efed7ebb81bcdc10ac
mirandaklein <mirandaklein@mac.com>
2019-09-16 16:52:58 -0600

    mouse over

--- designs.js
+++ designs.js
@@ -1,14 +1,17 @@
+const pixelCanvas = document.getElementById("pixelCanvas");
+
+
 //Changes cell based on color chosen
 function changeColor() {
   const color = document.getElementById("colorPicker").value;
   this.style.background = color;
 }
 
+
 //Makes initial grid on load, makes grid based on heigth x width, clears grid
 function makeGrid() {
   const gridHeight = document.getElementById("inputHeight").value;
   const gridWidth = document.getElementById("inputWidth").value;
-  const pixelCanvas = document.getElementById("pixelCanvas");
   pixelCanvas.innerText = ""; // empty table
 
   for (let h = 0; h < gridHeight; ++h) {
@@ -16,10 +19,16 @@ function makeGrid() {
     for (let w = 0; w < gridWidth; ++w) {
       const cell = row.insertCell(-1); //insert new cell at the last position
       cell.onclick = changeColor;
-    }
-  }
+      cell.onmouseover= changeColor;
+  };
+}
+
+    
+  
   event.preventDefault();
 }
+
+
 
 //Switch between light and dark mode
 let isDark = false;
