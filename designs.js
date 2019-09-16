6f159d5a087e3f267f67ff61545d9b595b3a4abd
mirandaklein <mirandaklein@mac.com>
2019-09-16 16:05:27 -0600

    light/dark mode

--- designs.js
+++ designs.js
@@ -1,31 +1,39 @@
-// Select color input
+//Changes cell based on color chosen
 function changeColor() {
-    const color = document.getElementById("colorPicker").value;
-    this.style.background = color;
+  const color = document.getElementById("colorPicker").value;
+  this.style.background = color;
 }
-// Select size input
 
-
-// When size is submitted by the user, call makeGrid()
-
+//Makes initial grid on load, makes grid based on heigth x width, clears grid
 function makeGrid() {
-    const gridHeight = document.getElementById("inputHeight").value;
-    const gridWidth = document.getElementById("inputWidth").value;
-    const pixelCanvas = document.getElementById("pixelCanvas"); 
-    pixelCanvas.innerText=""; // empty table   
-    
-    for (let h=0; h<gridHeight; ++h) {
-        const row = pixelCanvas.insertRow(-1); // insert new row at the last position
-        for (let w=0; w<gridWidth; ++w) {
-            const cell = row.insertCell(-1); //insert new cell at the last position
-            cell.onclick = changeColor;
-            
-        }
+  const gridHeight = document.getElementById("inputHeight").value;
+  const gridWidth = document.getElementById("inputWidth").value;
+  const pixelCanvas = document.getElementById("pixelCanvas");
+  pixelCanvas.innerText = ""; // empty table
+
+  for (let h = 0; h < gridHeight; ++h) {
+    const row = pixelCanvas.insertRow(-1); // insert new row at the last position
+    for (let w = 0; w < gridWidth; ++w) {
+      const cell = row.insertCell(-1); //insert new cell at the last position
+      cell.onclick = changeColor;
     }
-    event.preventDefault();
+  }
+  event.preventDefault();
 }
 
-function swapStyleSheet(sheet){
-	document.getElementById('pagestyle').setAttribute('href', sheet);
+//Switch between light and dark mode
+let isDark = false;
+console.log("first time isDark:", isDark)
+function clickStyleChange() {
+  const pageStyle = document.getElementById("pagestyle");
+  isDark = !isDark;
+  console.log("clicked isDark:", isDark)
+  if (isDark) {
+    pageStyle.setAttribute("href", "stylesDark.css");
+    return;
+  }
+  pageStyle.setAttribute("href", "styles.css");
 }
 
+// example
+//  isDark ? pageStyle.setAttribute("href", "stylesDark.css"): pageStyle.setAttribute("href", "styles.css");  
